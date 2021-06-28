4403762f47be4d4bd54e39d8194c3b6c00725314
kulahad <kulahad007@gmail.com>
2021-06-29 11:59:34 +1200

    fix: update position to absolute

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -140,7 +140,7 @@ const IndexPage = () => {
           />
           <a
             href="https://clte.fnu.ac.fj/talanoakaro"
-            className="z-10 flex items-end justify-start w-full h-full text-2xl text-white bg-gradient-to-t from-gray-700 hover:bg-gradient-to-b hover:from-gray-400 hover:text-black"
+            className="absolute z-10 flex items-end justify-start w-full h-full text-2xl text-white  bg-gradient-to-t from-gray-700 hover:bg-gradient-to-b hover:from-gray-400 hover:text-black"
           >
             <div className="m-8 text-lg font-normal">
               <h1 className="my-1 text-2xl ">Register Here</h1>
