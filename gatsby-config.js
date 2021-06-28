3e11eec3a22696499221fa066fdbea4970b8de08
kulahad <kulahad007@gmail.com>
2021-06-28 16:32:45 +1200

    fix: add configurations to `gatsby-config`

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,26 @@ module.exports = {
     title: "Community of Pasifika",
   },
   plugins: [
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        defaults: {
+          formats: [`auto`, `webp`],
+          placeholder: `dominantColor`,
+          quality: 50,
+          breakpoints: [750, 1080, 1366, 1920],
+          backgroundColor: `transparent`,
+          tracedSVGOptions: {},
+          blurredOptions: {},
+          jpgOptions: {},
+          pngOptions: {},
+          webpOptions: {},
+          avifOptions: {},
+        },
+      },
+    },
+    `gatsby-transformer-sharp`,
     "gatsby-plugin-postcss",
     "gatsby-transformer-remark",
     {
@@ -12,6 +32,11 @@ module.exports = {
         path: "./src/pages/",
       },
       __key: "pages",
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
     },
   ],
 };
