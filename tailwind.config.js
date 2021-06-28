8eb4af36ef789b8968c20418f476d98e044968ff
kulahad <kulahad007@gmail.com>
2021-06-28 16:40:12 +1200

    fix: extend tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#002147",
+        "light-grey": "#dee4e7",
+      },
+      fontFamily: {
+        primary: ["Crimson Text", "serif"],
+        secondary: ["Work Sans", "sans-serif"],
+      },
+      minHeight: {
+        "1/4-screen": "25vh",
+        "1/3-screen": "33vh",
+        "1/2-screen": "50vh",
+        "3/4-screen": "75vh",
+      },
+      maxHeight: {
+        "1/4-screen": "25vh",
+        "1/3-screen": "33vh",
+        "1/2-screen": "50vh",
+        "3/4-screen": "75vh",
+      },
+    },
   },
   variants: {
     extend: {},
