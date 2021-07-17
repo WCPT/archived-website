05f076ed13663a7f19b4ca180dbe1fcb1ecfb190
Hyun Kim <justinkimkorea@gmail.com>
2021-07-17 19:24:00 +1200

    fix: add back removed `Cousine` font family

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -17,7 +17,7 @@ export const Layout = ({ title, children }: Props) => {
           crossOrigin=""
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Bitter:ital,wght@0,300;0,400;0,500;1,300;1,400&family=Lato:wght@300;400;900&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Bitter:ital,wght@0,300;0,400;0,500;1,300;1,400&family=Lato:wght@300;400;900&family=Cousine:wght@700&display=swap"
           rel="stylesheet"
         />
       </Helmet>
