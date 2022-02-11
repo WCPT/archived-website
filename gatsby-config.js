9c9a3a52bac84a0671b98c2c1247c1318f3083bd
Hyun Kim <justinkimkorea@gmail.com>
2022-02-11 18:12:17 +1200

    chore: update stats

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,8 +15,8 @@ module.exports = {
     },
     stats: {
       engagements: "5000+",
-      registered: "2800+",
-      participants: "4065",
+      registered: "2900+",
+      participants: "4076",
       launched: "2021-06-17T00:00:00.000Z",
     },
   },
